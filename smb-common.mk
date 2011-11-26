cef6a27f00a391216bea9e57689325be0f58aa39
Dane Wagner <dane.wagner@gmail.com>
2011-11-26 16:08:21 -0600

    Build fixes for ICS
    Change-Id: I9ce514f3ec91d2a1cdbadf770d6a70c82f2faea9

--- smb-common.mk
+++ smb-common.mk
@@ -4,6 +4,7 @@ $(call inherit-product, build/target/product/full_base.mk)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init_recovery.rc:root/init_recovery.rc \
     $(LOCAL_PATH)/init.nvidia.rc:root/init.nvidia.rc
+    $(LOCAL_PATH)/initlogo.rle:root/initlogo.rle
 
 # Place wifi files
 PRODUCT_COPY_FILES += \
@@ -28,7 +29,7 @@ PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
     frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
-DEVICE_PACKAGE_OVERLAYS += device/malata/smb-common/overlay
+#DEVICE_PACKAGE_OVERLAYS += device/malata/smb-common/overlay
 
 # Include packages
 PRODUCT_PACKAGES += \
